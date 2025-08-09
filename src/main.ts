5ab54e2b0d1abf380d4eb1fe3450b70072bd4986
Hamza Mughal <33695480+iamhamzamughal@users.noreply.github.com>
2025-08-09 18:12:51 +0500

    update main

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as nodeCrypto from 'crypto';
+(global as any).crypto = nodeCrypto;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
